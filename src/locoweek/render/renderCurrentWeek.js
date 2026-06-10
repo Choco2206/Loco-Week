7f3ae9a02b551795ddaa05f6256f8199c02b8756
Choco2206 <k.alvaro@gmx.de>
2026-06-10 22:49:43 +0200

    Create renderCurrentWeek.js

---
+++ src/locoweek/render/renderCurrentWeek.js
@@ -0,0 +1,47 @@
+const { EmbedBuilder } = require('discord.js');
+
+function renderCurrentWeek() {
+  const embed = new EmbedBuilder()
+    .setColor('#c1121f')
+    .setImage(process.env.OVERVIEW_BANNER_URL || null)
+    .setDescription(
+      [
+        '## 📅 Aktuelle Woche',
+        '',
+        '**KW wird automatisch berechnet**',
+        '',
+        '**Montag**',
+        'Keine Termine',
+        '',
+        '**Dienstag**',
+        'Keine Termine',
+        '',
+        '**Mittwoch**',
+        'Keine Termine',
+        '',
+        '**Donnerstag**',
+        'Keine Termine',
+        '',
+        '**Freitag**',
+        'Keine Termine',
+        '',
+        '**Samstag**',
+        'Keine Termine',
+        '',
+        '**Sonntag**',
+        'Keine Termine',
+        '',
+        '‼️ **Allgemein gilt:** Bitte immer maximal **10 Minuten vor Beginn** eintreffen, damit keine Unruhe entsteht. ‼️'
+      ].join('\n')
+    )
+    .setTimestamp();
+
+  return {
+    embeds: [embed],
+    components: []
+  };
+}
+
+module.exports = {
+  renderCurrentWeek
+};
