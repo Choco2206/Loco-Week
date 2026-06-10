f9019ad6d397f66112777dcaf20c574010db304d
Choco2206 <k.alvaro@gmx.de>
2026-06-10 15:59:22 +0200

    Update writeStore.js

--- src/locoweek/store/writeStore.js
+++ src/locoweek/store/writeStore.js
@@ -1,1 +1,13 @@
+const fs = require('fs');
 
+function writeStore(filePath, data) {
+  fs.writeFileSync(
+    filePath,
+    JSON.stringify(data, null, 2),
+    'utf8'
+  );
+}
+
+module.exports = {
+  writeStore
+};
