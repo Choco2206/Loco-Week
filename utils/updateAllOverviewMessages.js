6f3a02c62591a632a6db7a1825bab172e72b3878
Choco2206 <k.alvaro@gmx.de>
2026-06-10 23:27:02 +0200

    Delete utils directory

--- utils/updateAllOverviewMessages.js
+++
@@ -1,9 +0,0 @@
-const updateOverviewMessage = require('./updateOverviewMessage');
-const updateNextWeekOverviewMessage = require('./updateNextWeekOverviewMessage');
-
-async function updateAllOverviewMessages(client) {
-  await updateOverviewMessage(client);
-  await updateNextWeekOverviewMessage(client);
-}
-
-module.exports = updateAllOverviewMessages;
