48c15749c7661f70bce7e0c714022ad9224cc327
Choco2206 <k.alvaro@gmx.de>
2026-06-10 15:58:53 +0200

    Update ensureStore.js

--- src/locoweek/store/ensureStore.js
+++ src/locoweek/store/ensureStore.js
@@ -1,1 +1,69 @@
+const fs = require('fs');
 
+const {
+  DATA_DIR,
+  CURRENT_WEEK_FILE,
+  NEXT_WEEK_FILE,
+  EVENT_TYPES_FILE,
+  TEAMS_FILE,
+  SETTINGS_FILE,
+  MESSAGES_FILE
+} = require('./paths');
+
+function writeJsonIfMissing(filePath, defaultData) {
+  if (fs.existsSync(filePath)) return;
+
+  fs.writeFileSync(
+    filePath,
+    JSON.stringify(defaultData, null, 2),
+    'utf8'
+  );
+}
+
+function ensureStore() {
+  if (!fs.existsSync(DATA_DIR)) {
+    fs.mkdirSync(DATA_DIR, { recursive: true });
+  }
+
+  writeJsonIfMissing(CURRENT_WEEK_FILE, {
+    weekOffset: 0,
+    entries: []
+  });
+
+  writeJsonIfMissing(NEXT_WEEK_FILE, {
+    weekOffset: 1,
+    entries: []
+  });
+
+  writeJsonIfMissing(EVENT_TYPES_FILE, {
+    types: [
+      'Freundschaftsspiel',
+      'PL',
+      'VPG',
+      'PLA',
+      'RPL',
+      'PL International',
+      'Cup',
+      'Sonstiges'
+    ]
+  });
+
+  writeJsonIfMissing(TEAMS_FILE, {
+    teams: {}
+  });
+
+  writeJsonIfMissing(SETTINGS_FILE, {
+    timezone: 'Europe/Berlin'
+  });
+
+  writeJsonIfMissing(MESSAGES_FILE, {
+    adminPanelMessageId: '',
+    currentWeekMessageId: '',
+    nextWeekPreviewMessageId: '',
+    headerMessageId: ''
+  });
+}
+
+module.exports = {
+  ensureStore
+};
