df3c4091a3faa905cb35f56c3ed35c376175963f
Choco2206 <k.alvaro@gmx.de>
2026-06-13 21:51:01 +0200

    renderNextWeek.js aktualisieren

--- src/locoweek/render/renderNextWeek.js
+++ src/locoweek/render/renderNextWeek.js
@@ -12,9 +12,12 @@ function renderNextWeek() {
   const weekInfo = getWeekInfo(1);
 
   const lines = [
-    `## KW ${weekInfo.weekNumber} • ${weekInfo.startDate} - ${weekInfo.endDate}`,
-    ''
-  ];
+  `## KW ${weekInfo.weekNumber} • ${weekInfo.startDate} - ${weekInfo.endDate}`,
+  '',
+  '‼️ **Allgemein gilt:**',
+  'Bitte immer maximal **10 Minuten vor Beginn** eintreffen, damit keine Unruhe entsteht.',
+  '',
+];
 
   for (const day of DAYS) {
     lines.push(`**${day.label}**`);
@@ -35,10 +38,6 @@ function renderNextWeek() {
 
     lines.push('');
   }
-
-  lines.push(
-    '‼️ **Allgemein gilt:** Bitte immer maximal **10 Minuten vor Beginn** eintreffen, damit keine Unruhe entsteht. ‼️'
-  );
 
   const embed = new EmbedBuilder()
     .setColor('#c1121f')
