5fe3bb453d14d6ecb0ca1c6c9bcae85e51e28993
Choco2206 <k.alvaro@gmx.de>
2026-06-13 21:15:32 +0200

    writeStore.js aktualisieren

--- src/locoweek/store/writeStore.js
+++ src/locoweek/store/writeStore.js
@@ -1,18 +1,13 @@
-const fs = require("fs");
-const { ensureStore } = require("./ensureStore");
-const {
-  CURRENT_WEEK_FILE,
-  NEXT_WEEK_FILE,
-  SETTINGS_FILE
-} = require("./paths");
+const fs = require('fs');
 
-function writeJson(filePath, data) {
-  ensureStore();
-  fs.writeFileSync(filePath, JSON.stringify(data, null, 2), "utf8");
+function writeStore(filePath, data) {
+  fs.writeFileSync(
+    filePath,
+    JSON.stringify(data, null, 2),
+    'utf8'
+  );
 }
 
 module.exports = {
-  writeCurrentWeek: (data) => writeJson(CURRENT_WEEK_FILE, data),
-  writeNextWeek: (data) => writeJson(NEXT_WEEK_FILE, data),
-  writeSettings: (data) => writeJson(SETTINGS_FILE, data)
+  writeStore
 };
