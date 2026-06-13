8d3d556a313dff3390c5e89d4ca7982cf7f9ea82
Choco2206 <k.alvaro@gmx.de>
2026-06-13 22:10:36 +0200

    ensureStore.js aktualisieren

--- src/locoweek/store/ensureStore.js
+++ src/locoweek/store/ensureStore.js
@@ -3,6 +3,8 @@ const {
   DATA_DIR,
   CURRENT_WEEK_FILE,
   NEXT_WEEK_FILE,
+  EVENT_TYPES_FILE,
+  TEAMS_FILE,
   SETTINGS_FILE
 } = require("./paths");
 
@@ -25,6 +27,36 @@ function ensureJsonFile(filePath, defaultData) {
 function ensureStore() {
   ensureJsonFile(CURRENT_WEEK_FILE, getEmptyWeek());
   ensureJsonFile(NEXT_WEEK_FILE, getEmptyWeek());
+
+  ensureJsonFile(EVENT_TYPES_FILE, {
+    types: [
+      { name: "PL", needsOpponent: true },
+      { name: "VPG", needsOpponent: true },
+      { name: "RPL", needsOpponent: true },
+      { name: "PLA", needsOpponent: true },
+      { name: "PL International", needsOpponent: true },
+      { name: "Freundschaftsspiel", needsOpponent: true },
+
+      { name: "Level Session", needsOpponent: false },
+      { name: "Loco Night Cup", needsOpponent: false },
+      { name: "Pad Cup", needsOpponent: false },
+      { name: "Reki Cup", needsOpponent: false },
+      { name: "Wizard Cup", needsOpponent: false },
+      { name: "Apex Cup", needsOpponent: false },
+      { name: "Bomber Cup", needsOpponent: false },
+      { name: "Among Us", needsOpponent: false },
+      { name: "Minecraft", needsOpponent: false },
+      { name: "Poker", needsOpponent: false },
+      { name: "BRFC Nightcup", needsOpponent: false },
+      { name: "GTA Online", needsOpponent: false },
+      { name: "Rocket League", needsOpponent: false },
+      { name: "Sonstiges", needsOpponent: false }
+    ]
+  });
+
+  ensureJsonFile(TEAMS_FILE, {
+    teams: []
+  });
 
   ensureJsonFile(SETTINGS_FILE, {
     weekChannelId: "",
